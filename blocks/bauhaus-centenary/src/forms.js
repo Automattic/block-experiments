2a43df245d734577206b3939a34f6c879ae886bb
Alex Lende <ajlende@gmail.com>
2019-12-17 13:02:59 -0600

    Remove extra colors

--- blocks/bauhaus-centenary/src/forms.js
+++ blocks/bauhaus-centenary/src/forms.js
@@ -6,13 +6,12 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import * as Icon from './icon';
-import colors from './colors';
 
 const forms = {
 	triangle: Icon.FormTriangle,
@@ -32,29 +31,6 @@ const Forms = ( { className, attributes } ) => {
 };
 
 const Content = Forms;
-
-const labels = {
-	triangle: __( 'Triangle' ),
-	square: __( 'Square' ),
-	circle: __( 'Circle' ),
-};
-
-const extraColors = ( { setAttributes, attributes } ) =>
-	// TODO: Check to see if we need to support flatMap on IE11
-	attributes.forms.flatMap( ( form, index ) => [
-		{
-			colors,
-			value: form.fill,
-			onChange: ( fill ) => setAttributes( ( prevForms ) => prevForms.splice( index, 1, { ...form, fill } ) ),
-			label: sprintf( __( '%s Fill [%d]' ), labels[ form.type ], index + 1 ),
-		},
-		{
-			colors,
-			value: form.stroke,
-			onChange: ( stroke ) => setAttributes( ( prevForms ) => prevForms.splice( index, 1, { ...form, stroke } ) ),
-			label: sprintf( __( '%s Stroke [%d]' ), labels[ form.type ], index + 1 ),
-		},
-	] );
 
 export default Object.assign( Forms, {
 	label: __( 'Forms' ),
