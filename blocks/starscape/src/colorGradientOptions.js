75f36251442a922a5fb76b197b42f5d08352c203
Alex Lende <ajlende@gmail.com>
2020-01-21 16:35:10 -0600

    Simplify gradient/color options

--- blocks/starscape/src/colorGradientOptions.js
+++ blocks/starscape/src/colorGradientOptions.js
@@ -10,103 +10,51 @@ export default {
 			gradient: '#00000c',
 		},
 		{
-			name: __( 'One AM' ),
-			gradient: 'linear-gradient(141deg, #020111 85%,#191621 100%)',
-		},
-		{
-			name: __( 'Two AM' ),
+			name: __( 'Astronomical Dawn' ),
 			gradient: 'linear-gradient(141deg, #020111 60%,#20202c 100%)',
 		},
 		{
-			name: __( 'Three AM' ),
+			name: __( 'Nautical Dawn' ),
 			gradient: 'linear-gradient(141deg, #020111 10%,#3a3a52 100%)',
 		},
 		{
-			name: __( 'Four AM' ),
+			name: __( 'Civil Dawn' ),
 			gradient: 'linear-gradient(141deg, #20202c 0%,#515175 100%)',
 		},
 		{
-			name: __( 'Five AM' ),
-			gradient: 'linear-gradient(141deg, #40405c 0%,#6f71aa 80%,#8a76ab 100%)',
+			name: __( 'Sunrise' ),
+			gradient: 'linear-gradient(141deg, #20202c 0%,#6f71aa 80%,#8a76ab 100%)',
 		},
 		{
-			name: __( 'Six AM' ),
-			gradient: 'linear-gradient(141deg, #4a4969 0%,#7072ab 50%,#cd82a0 100%)',
+			name: __( 'Morning' ),
+			gradient: 'linear-gradient(141deg, #40405c 0%,#7072ab 50%,#cd82a0 100%)',
 		},
 		{
-			name: __( 'Seven AM' ),
-			gradient: 'linear-gradient(141deg, #757abf 0%,#8583be 60%,#eab0d1 100%)',
+			name: __( 'Atmosphere' ),
+			gradient: 'linear-gradient(180deg, #00000c 65%,#1b2741 85%,#2e4473 95%,#4463a3 100%)',
 		},
 		{
-			name: __( 'Eight AM' ),
-			gradient: 'linear-gradient(141deg, #82addb 0%,#ebb2b1 100%)',
+			name: __( 'Astronomical Dusk' ),
+			gradient: 'linear-gradient(141deg, #0b050b 60%,#27171c 100%)',
 		},
 		{
-			name: __( 'Nine AM' ),
-			gradient: 'linear-gradient(141deg, #94c5f8 1%,#a6e6ff 70%,#b1b5ea 100%)',
+			name: __( 'Nautical Dusk' ),
+			gradient: 'linear-gradient(141deg, #0b050b 10%,#4c2d2f 100%)',
 		},
 		{
-			name: __( 'Ten AM' ),
-			gradient: 'linear-gradient(141deg, #b7eaff 0%,#94dfff 100%)',
+			name: __( 'Civil Dusk' ),
+			gradient: 'linear-gradient(141deg, #130f13 0%,#2f2122 50%,#6c353a 100%)',
 		},
 		{
-			name: __( 'Eleven AM' ),
-			gradient: 'linear-gradient(141deg, #9be2fe 0%,#67d1fb 100%)',
+			name: __( 'Sunset' ),
+			gradient: 'linear-gradient(141deg, #1e1818 0%,#2f2122 30%,#6c353a 70%,#cf804b 100%)',
 		},
 		{
-			name: __( 'Noon' ),
-			gradient: 'linear-gradient(141deg, #90dffe 0%,#38a3d1 100%)',
-		},
-		{
-			name: __( 'One PM' ),
-			gradient: 'linear-gradient(141deg, #57c1eb 0%,#246fa8 100%)',
-		},
-		{
-			name: __( 'Two PM' ),
-			gradient: 'linear-gradient(141deg, #2d91c2 0%,#1e528e 100%)',
-		},
-		{
-			name: __( 'Three PM' ),
-			gradient: 'linear-gradient(141deg, #2473ab 0%,#1e528e 70%,#5b7983 100%)',
-		},
-		{
-			name: __( 'Four PM' ),
-			gradient: 'linear-gradient(141deg, #1e528e 0%,#265889 50%,#9da671 100%)',
-		},
-		{
-			name: __( 'Five PM' ),
-			gradient: 'linear-gradient(141deg, #1e528e 0%,#728a7c 50%,#e9ce5d 100%)',
-		},
-		{
-			name: __( 'Six PM' ),
-			gradient: 'linear-gradient(141deg, #154277 0%,#576e71 30%,#e1c45e 70%,#b26339 100%)',
-		},
-		{
-			name: __( 'Seven PM' ),
-			gradient: 'linear-gradient(141deg, #163C52 0%,#4F4F47 30%,#C5752D 60%,#B7490F 80%, #2F1107 100%)',
-		},
-		{
-			name: __( 'Eight PM' ),
-			gradient: 'linear-gradient(141deg, #071B26 0%,#071B26 30%,#8A3B12 80%,#240E03 100%)',
-		},
-		{
-			name: __( 'Nine PM' ),
-			gradient: 'linear-gradient(141deg, #010A10 30%,#59230B 80%,#2F1107 100%)',
-		},
-		{
-			name: __( 'Ten PM' ),
-			gradient: 'linear-gradient(141deg, #090401 50%,#4B1D06 100%)',
-		},
-		{
-			name: __( 'Eleven PM' ),
-			gradient: 'linear-gradient(141deg, #00000c 80%,#150800 100%)',
+			name: __( 'Evening' ),
+			gradient: 'linear-gradient(141deg, #2f2122 10%,#6c353a 40%,#cf804b 80%,#ffeb59 100%)',
 		},
 	],
 	colors: [
-		{
-			name: __( 'Black' ),
-			color: '#000000',
-		},
 		{
 			name: __( 'White' ),
 			color: '#ffffff',
