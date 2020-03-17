a9d1968336225b666d839828cfb4a6d74592d42d
Marcus Kazmierczak <marcus@mkaz.com>
2020-03-17 14:08:28 -0700

    Update license blurb

--- blocks/image-compare/assets/juxtapose.js
+++ blocks/image-compare/assets/juxtapose.js
@@ -1,7 +1,13 @@
 /**
- * Original Juxtapose code licensed under MPLv2
- * juxtapose - v1.2.0 - 2017-12-18
+ * Juxtapose - v1.2.0 - 2017-12-18
  * Copyright (c) 2017 Alex Duner and Northwestern University Knight Lab
+ *
+ * This code is governed by the terms of the Mozilla Public License (MPL) v2.0,
+ * which is available here: https://github.com/NUKnightLab/juxtapose/blob/master/LICENSE
+ *
+ * This code is incorporated into Image Compare plugin, which is licensed under
+ * GPLv2+, however you may use Juxtapose code separately under the terms of its
+ * original MPL 2.0 license if you wish.
  */
 
 wp.domReady( function() {
