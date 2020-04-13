8f39d7b25106a719bdf6b8514f1e09e68a99b380
Joen Asmussen <1204802+jasmussen@users.noreply.github.com>
2020-04-13 12:39:26 +0200

    Add new icons. (#73)
    * Add new icons.
    
    * Square resize handles and new gutter style.
    
    * Add end gutter indication.

--- blocks/layout-grid/src/icons.js
+++ blocks/layout-grid/src/icons.js
@@ -9,7 +9,16 @@ export const GridIcon = () => (
 		width="24" height="24"
 		viewBox="0 0 24 24"
 	>
-		<Path d="M4 5v13h17V5H4zm10 2v9h-3V7h3zM6 7h3v9H6V7zm13 9h-3V7h3v9z" />
+		<Path d="M19 6H6c-1.1 0-2 .9-2 2v9c0 1.1.9 2 2 2h13c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-7.5 11.5H6c-.3 0-.5-.2-.5-.5V8c0-.3.2-.5.5-.5h5.5v10zm4 0H13v-10h2.5v10zm4-.5c0 .3-.2.5-.5.5h-2v-10h2c.3 0 .5.2.5.5v9z" />
+	</SVG>
+);
+
+export const GridColumnIcon = () => (
+	<SVG xmlns="http://www.w3.org/2000/svg"
+		width="24" height="24"
+		viewBox="0 0 24 24"
+	>
+		<Path d="M19 6H6c-1.1 0-2 .9-2 2v9c0 1.1.9 2 2 2h13c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zM5.5 17V8c0-.3.2-.5.5-.5h5.5v10H6c-.3 0-.5-.2-.5-.5zm14 0c0 .3-.2.5-.5.5h-2v-10h2c.3 0 .5.2.5.5v9z" />
 	</SVG>
 );
 
