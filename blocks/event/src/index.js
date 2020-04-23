5827ec0f024601a34c7cc79a34b6ac2cc114c066
Alex Lende <ajlende@gmail.com>
2020-04-22 23:53:59 -0500

    Add block example

--- blocks/event/src/index.js
+++ blocks/event/src/index.js
@@ -24,6 +24,23 @@ export function registerBlock() {
 		supports: {
 			align: [ 'center', 'wide' ],
 		},
+		example: {
+			attributes: {
+				eventTitle: 'Digging for Treasure',
+				eventLocation: 'Treasure Island',
+				eventStart: '1783-11-14T12:00:00',
+				eventImageId: 1,
+				eventImageURL:
+					'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/TI-treasure.jpg/366px-TI-treasure.jpg',
+				eventImageAlt: 'Treasure',
+				focalPoint: {
+					x: '0.30',
+					y: '0.40',
+				},
+				customBackgroundColor: '#d4c5af',
+				align: 'wide',
+			},
+		},
 		attributes: {
 			eventTitle: {
 				type: 'string',
