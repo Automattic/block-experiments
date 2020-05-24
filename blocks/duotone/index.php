930da5ca89ecea2fe3418638733dd922dd393b0a
Alex Lende <ajlende@gmail.com>
2020-05-24 16:56:57 -0600

    code style

--- blocks/duotone/index.php
+++ blocks/duotone/index.php
@@ -9,16 +9,16 @@ add_action( 'init', function() {
 
 	function is_supported_block( $block ) {
 		$supported_blocks = [ 'core/image', 'core/cover' ];
-		return in_array( $block[ 'blockName' ], $supported_blocks );
+		return in_array( $block['blockName'], $supported_blocks );
 	}
 
 	function hex2rgb( $color ) {
 		$color = trim( $color, '#' );
-		if ( strlen( $color ) == 3 ) {
+		if ( strlen( $color ) === 3 ) {
 			$r = hexdec( substr( $color, 0, 1 ) . substr( $color, 0, 1 ) );
 			$g = hexdec( substr( $color, 1, 1 ) . substr( $color, 1, 1 ) );
 			$b = hexdec( substr( $color, 2, 1 ) . substr( $color, 2, 1 ) );
-		} elseif ( strlen( $color ) == 6 ) {
+		} elseif ( strlen( $color ) === 6 ) {
 			$r = hexdec( substr( $color, 0, 2 ) );
 			$g = hexdec( substr( $color, 2, 2 ) );
 			$b = hexdec( substr( $color, 4, 2 ) );
@@ -32,13 +32,14 @@ add_action( 'init', function() {
 		);
 	}
 
-	add_filter( 'render_block', function( $block_content, $block ) {
+	add_filter( 'render_block', function ( $block_content, $block ) {
 		if ( ! is_supported_block( $block ) ) {
 			return $block_content;
 		}
-		$duotoneId = $block[ 'attrs' ][ 'duotoneId' ];
-		$duotoneDark = hex2rgb( $block[ 'attrs' ][ 'duotoneDark' ] );
-		$duotoneLight = hex2rgb( $block[ 'attrs' ][ 'duotoneLight' ] );
+
+		$duotone_id = $block['attrs']['duotoneId'];
+		$duotone_dark = hex2rgb( $block['attrs']['duotoneDark'] );
+		$duotone_light = hex2rgb( $block['attrs']['duotoneLight'] );
 		$duotone = <<<EOT
 <svg
 	xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -48,7 +49,7 @@ add_action( 'init', function() {
 	role="none"
 	>
 	<defs>
-		<filter id="$duotoneId">
+		<filter id="$duotone_id">
 			<feColorMatrix
 				type="matrix"
 				values=".2989 .5870 .1140 0 0
@@ -57,9 +58,9 @@ add_action( 'init', function() {
 				        0 0 0 1 0"
 			/>
 			<feComponentTransfer color-interpolation-filters="sRGB">
-				<feFuncR type="table" tableValues="{$duotoneDark[ 'r' ]} {$duotoneLight[ 'r' ]}" />
-				<feFuncG type="table" tableValues="{$duotoneDark[ 'g' ]} {$duotoneLight[ 'g' ]}" />
-				<feFuncB type="table" tableValues="{$duotoneDark[ 'b' ]} {$duotoneLight[ 'b' ]}" />
+				<feFuncR type="table" tableValues="{$duotone_dark['r']} {$duotone_light['r']}" />
+				<feFuncG type="table" tableValues="{$duotone_dark['g']} {$duotone_light['g']}" />
+				<feFuncB type="table" tableValues="{$duotone_dark['b']} {$duotone_light['b']}" />
 			</feComponentTransfer>
 		</filter>
 	</defs>
