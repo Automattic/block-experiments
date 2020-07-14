b6bca4ea9009ebff5c7f1d3301297d5bf933baa2
John Godley <johng75@gmail.com>
2020-07-14 09:33:44 +0100

    Fix loading of style.css inside the editor

--- blocks/layout-grid/index.php
+++ blocks/layout-grid/index.php
@@ -3,22 +3,22 @@
 add_action( 'init', function() {
 	register_block_type( 'jetpack/layout-grid', [
 		'editor_script' => 'block-experiments',
+		'style' => 'block-experiments',
 		'editor_style' => 'block-experiments-editor',
 		'render_callback' => function( $attribs, $content ) {
-			wp_enqueue_style( 'jetpack-layout-grid' );
 			wp_enqueue_style( 'wpcom-layout-grid-front' );
 			return $content;
-		}
+		},
 	] );
 
 	register_block_type( 'jetpack/layout-grid-column', [
 		'editor_script' => 'block-experiments',
+		'style' => 'block-experiments',
 		'editor_style' => 'block-experiments-editor',
 		'render_callback' => function( $attribs, $content ) {
-			wp_enqueue_style( 'jetpack-layout-grid' );
 			wp_enqueue_style( 'wpcom-layout-grid-front' );
 			return $content;
-		}
+		},
 	] );
 
 	wp_set_script_translations( 'jetpack/layout-grid', 'layout-grid' );
