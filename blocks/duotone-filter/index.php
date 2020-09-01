a2e50b26f690678f8c09fb76a821c404dc668451
Alex Lende <ajlende@gmail.com>
2020-09-01 13:25:05 -0500

    Code cleanup

--- blocks/duotone-filter/index.php
+++ blocks/duotone-filter/index.php
@@ -9,63 +9,23 @@ add_action( 'init', function() {
 
 	function is_supported_block( $block ) {
 		$supported_blocks = [ 'core/image', 'core/cover' ];
-		return in_array( $block['blockName'], $supported_blocks );
-	}
-
-	function hex2rgb( $color ) {
-		$color = trim( $color, '#' );
-		if ( strlen( $color ) === 3 ) {
-			$r = hexdec( substr( $color, 0, 1 ) . substr( $color, 0, 1 ) );
-			$g = hexdec( substr( $color, 1, 1 ) . substr( $color, 1, 1 ) );
-			$b = hexdec( substr( $color, 2, 1 ) . substr( $color, 2, 1 ) );
-		} elseif ( strlen( $color ) === 6 ) {
-			$r = hexdec( substr( $color, 0, 2 ) );
-			$g = hexdec( substr( $color, 2, 2 ) );
-			$b = hexdec( substr( $color, 4, 2 ) );
-		} else {
-			return array();
-		}
-		return array(
-			'r' => $r / 255,
-			'g' => $g / 255,
-			'b' => $b / 255,
-		);
+		return in_array( $block['blockName'], $supported_blocks, true );
 	}
 
 	add_filter( 'render_block', function ( $block_content, $block ) {
-		if ( ! is_supported_block( $block ) ) {
+		if (
+			! is_supported_block( $block ) ||
+			! array_key_exists( 'duotoneId', $block['attrs'] ) ||
+			! array_key_exists( 'duotoneDark', $block['attrs'] ) ||
+			! array_key_exists( 'duotoneLight', $block['attrs'] )
+		) {
 			return $block_content;
 		}
 
-		$duotone_id = $block['attrs']['duotoneId'];
-		$duotone_dark = hex2rgb( $block['attrs']['duotoneDark'] );
-		$duotone_light = hex2rgb( $block['attrs']['duotoneLight'] );
-		$duotone = <<<EOT
-<svg
-	xmlns:xlink="http://www.w3.org/1999/xlink"
-	style="visibility: hidden !important; position: absolute !important; left: -9999px !important; overflow: hidden !important;"
-	aria-hidden="true"
-	focusable="false"
-	role="none"
-	>
-	<defs>
-		<filter id="$duotone_id">
-			<feColorMatrix
-				type="matrix"
-				values=".2989 .5870 .1140 0 0
-				        .2989 .5870 .1140 0 0
-				        .2989 .5870 .1140 0 0
-				        0 0 0 1 0"
-			/>
-			<feComponentTransfer color-interpolation-filters="sRGB">
-				<feFuncR type="table" tableValues="{$duotone_dark['r']} {$duotone_light['r']}" />
-				<feFuncG type="table" tableValues="{$duotone_dark['g']} {$duotone_light['g']}" />
-				<feFuncB type="table" tableValues="{$duotone_dark['b']} {$duotone_light['b']}" />
-			</feComponentTransfer>
-		</filter>
-	</defs>
-</svg>
-EOT; 
-		return $block_content . $duotone;
+		ob_start();
+		include 'duotone.php';
+		$duotone = ob_get_clean();
+
+		return $duotone . $block_content;
 	}, 10, 2 );
 } );
--- blocks/duotone-filter/src/index.js
+++ blocks/duotone-filter/src/index.js
@@ -7,34 +7,15 @@ import { useEffect } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import Duotone from './duotone';
+
 const SUPPORTED_BLOCKS = [ 'core/image', 'core/cover' ];
 
 export const isSupportedBlock = ( blockName ) =>
 	SUPPORTED_BLOCKS.includes( blockName );
-
-const parseColor = ( color ) => {
-	if ( ! color ) return {};
-
-	let r = '0';
-	let g = '0';
-	let b = '0';
-
-	if ( color.length === 7 ) {
-		r = '0x' + color[ 1 ] + color[ 2 ];
-		g = '0x' + color[ 3 ] + color[ 4 ];
-		b = '0x' + color[ 5 ] + color[ 6 ];
-	} else if ( color.length === 4 ) {
-		r = '0x' + color[ 1 ] + color[ 1 ];
-		g = '0x' + color[ 2 ] + color[ 2 ];
-		b = '0x' + color[ 3 ] + color[ 3 ];
-	}
-
-	return {
-		r: r / 0xff,
-		g: g / 0xff,
-		b: b / 0xff,
-	};
-};
 
 const withDuotoneAttributes = ( settings, blockName ) => {
 	if ( isSupportedBlock( blockName ) ) {
@@ -62,8 +43,6 @@ const withDuotoneEditorControls = createHigherOrderComponent(
 		}
 
 		const instanceId = useInstanceId( BlockEdit );
-		const duotoneDark = parseColor( attributes.duotoneDark );
-		const duotoneLight = parseColor( attributes.duotoneLight );
 
 		useEffect( () => {
 			setAttributes( {
@@ -93,51 +72,26 @@ const withDuotoneEditorControls = createHigherOrderComponent(
 						] }
 					/>
 				</InspectorControls>
-				<svg
-					viewBox="0 0 0 0"
-					width="0"
-					height="0"
-					xmlnsXlink="http://www.w3.org/1999/xlink"
-					style={ {
-						visibility: 'hidden',
-						position: 'absolute',
-						left: '-9999px',
-						overflow: 'hidden',
-					} }
-					ariaHidden="true"
-					focusable="false"
-					role="none"
-				>
-					<defs>
-						<filter id={ attributes.duotoneId }>
-							<feColorMatrix
-								type="matrix"
-								// prettier-ignore
-								values=".2989 .5870 .1140 0 0
-								        .2989 .5870 .1140 0 0
-								        .2989 .5870 .1140 0 0
-								        0 0 0 1 0"
-							/>
-							<feComponentTransfer colorInterpolationFilters="sRGB">
-								<feFuncR
-									type="table"
-									tableValues={ `${ duotoneDark.r } ${ duotoneLight.r }` }
-								/>
-								<feFuncG
-									type="table"
-									tableValues={ `${ duotoneDark.g } ${ duotoneLight.g }` }
-								/>
-								<feFuncB
-									type="table"
-									tableValues={ `${ duotoneDark.b } ${ duotoneLight.b }` }
-								/>
-							</feComponentTransfer>
-						</filter>
-					</defs>
-				</svg>
-				<div style={ { filter: `url( #${ attributes.duotoneId } )` } }>
+				{ attributes.duotoneDark &&
+				attributes.duotoneLight &&
+				attributes.duotoneId ? (
+					<>
+						<Duotone
+							id={ attributes.duotoneId }
+							darkColor={ attributes.duotoneDark }
+							lightColor={ attributes.duotoneLight }
+						/>
+						<div
+							style={ {
+								filter: `url( #${ attributes.duotoneId } )`,
+							} }
+						>
+							<BlockEdit { ...props } />
+						</div>
+					</>
+				) : (
 					<BlockEdit { ...props } />
-				</div>
+				) }
 			</>
 		);
 	},
@@ -145,8 +99,17 @@ const withDuotoneEditorControls = createHigherOrderComponent(
 );
 
 function addDuotoneFilterStyle( props, block, attributes ) {
-	if ( ! isSupportedBlock( block.name ) ) return props;
+	if (
+		! isSupportedBlock( block.name ) ||
+		! attributes.duotoneDark ||
+		! attributes.duotoneLight ||
+		! attributes.duotoneId
+	) {
+		return props;
+	}
+
 	const { style = {} } = props;
+
 	return { style: { ...style, filter: `url( #${ attributes.duotoneId } )` } };
 }
 
