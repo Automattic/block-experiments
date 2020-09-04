987be1086df76e6ef47daee6e90b880d79f10974
Enej Bajgoric <enej.bajgoric@automattic.com>
2020-09-04 15:09:26 -0700

    Add Gutter Settings

--- blocks/layout-grid/src/grid/edit.native.js
+++ blocks/layout-grid/src/grid/edit.native.js
@@ -7,13 +7,26 @@ import { delay, flatMap } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { InnerBlocks, BlockVariationPicker } from '@wordpress/block-editor';
-
+import { 
+	InnerBlocks,
+	InspectorControls,
+	BlockVariationPicker,
+ } from '@wordpress/block-editor';
+ import {
+	PanelBody,
+	SelectControl,
+	ToggleControl,
+	FooterMessageControl,
+	Disabled,
+} from '@wordpress/components';
 import { useEffect, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
 import variations from './variations';
+import { getGutterValues } from './../constants';
+
 
 const ALLOWED_BLOCKS = [ 'jetpack/layout-grid-column' ];
 
@@ -23,9 +36,11 @@ const TEMPLATE = [
 ];
 
 const Edit = ( props ) => {
-	const { clientId, isSelected } = props;
+	const { clientId, isSelected, setAttributes, attributes = {} } = props;
 	const [ isVisible, setIsVisible ] = useState( false );
 	const isDefaultColumns = true;
+
+	const { gutterSize, addGutterEnds, verticalAlignment } = attributes;
 
 	useEffect( () => {
 		if ( isSelected && isDefaultColumns ) {
@@ -33,6 +48,17 @@ const Edit = ( props ) => {
 		}
 	}, [] );
 
+	const toggleControl = (
+		<ToggleControl
+			label={ __( 'Add end gutters', 'layout-grid' ) }
+			help={
+				addGutterEnds ? __( 'Toggle off to remove the spacing left and right of the grid.', 'layout-grid' ) : __( 'Toggle on to add space left and right of the layout grid. ', 'layout-grid' )
+			}
+			checked={ addGutterEnds }
+			onChange={ newValue => setAttributes( { addGutterEnds: newValue } )  }
+		/>
+	);
+	
 	return (
 		<>
 			<View>
@@ -42,6 +68,29 @@ const Edit = ( props ) => {
 					allowedBlocks={ ALLOWED_BLOCKS }
 				/>
 			</View>
+			<InspectorControls>
+				<PanelBody title={ __( 'Layout grid settings' ) }>
+				<SelectControl
+					label={ __( 'Gutters' ) }
+					value={ gutterSize }
+					// `undefined` is required for the preload attribute to be unset.
+					onChange={  newValue => setAttributes( { gutterSize: newValue, addGutterEnds: newValue === 'none' ? false : addGutterEnds } ) }
+					options={ getGutterValues() }
+				/>
+				{ gutterSize === 'none' ? (
+					<Disabled>
+						{ toggleControl }
+					</Disabled>
+				) : toggleControl }
+				</PanelBody>
+				<PanelBody>
+					<FooterMessageControl
+						label={ __(
+							'Note: Column layout may vary between themes and screen sizes'
+						) }
+					/>
+				</PanelBody>
+			</InspectorControls>
 			<BlockVariationPicker
 				variations={ variations }
 				onClose={ () => setIsVisible( false ) }
