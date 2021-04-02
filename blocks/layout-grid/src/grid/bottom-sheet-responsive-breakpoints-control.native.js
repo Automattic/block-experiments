b8e942aebfb12a2c8774421b48e55e25afbea994
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-04-02 10:29:35 -0700

    Update all the controls
    Make the controls work.
    - Responsive Breakpoint controls.
    - Bottom Variation Controls
    - Single Column Controls ( still missing the colour Control)

--- blocks/layout-grid/src/grid/bottom-sheet-responsive-breakpoints-control.native.js
+++ blocks/layout-grid/src/grid/bottom-sheet-responsive-breakpoints-control.native.js
@@ -11,10 +11,18 @@ import { useState } from '@wordpress/element';
 import { Icon, chevronRight, check } from '@wordpress/icons';
 import { __, sprintf } from '@wordpress/i18n';
 import { BottomSheet, SegmentedControls, RangeControl, } from '@wordpress/components';
-import { columns } from '@wordpress/icons';
+
+import { getGridWidth, getDefaultSpan } from './grid-defaults';
 /**
  * Internal dependencies
  */
+ import {
+	DEVICE_BREAKPOINTS,
+	getSpanForDevice,
+	getOffsetForDevice,
+	getLayouts,
+} from './../constants';
+import LayoutGrid from './layout-grid';
 import styles from './edit.native.scss';
 
 const BottomSheetResponsiveBreakpointsControl = ( {
@@ -22,62 +30,100 @@ const BottomSheetResponsiveBreakpointsControl = ( {
 	options: items,
 	onChange,
 	value: selectedValue,
+	columns: numberOfColumns,
 } ) => {
 	const [ showSubSheet, setShowSubSheet ] = useState( false );
+	const [ currentDevice, setCurrentDevice ] = useState(
+		DEVICE_BREAKPOINTS[ 0 ]
+	); // Default current device. "Desktop"
+
 	const navigation = useNavigation();
 
-	const onChangeValue = ( value ) => {
-		return () => {
-			goBack();
-			onChange( value );
-		};
-	};
-
+	/**
+	 * Brings you back to the original settings.
+	 */
 	const goBack = () => {
 		setShowSubSheet( false );
 		navigation.goBack();
 	};
 
+	/**
+	 * Runs after you Click Edit.
+	 */
 	const openSubSheet = () => {
 		navigation.navigate( BottomSheet.SubSheet.screenName );
 		setShowSubSheet( true );
 	};
+	/**
+	 * Helper function that passed back attributes key and value to updated.
+	 *
+	 * @param {string} key
+	 * @param {string} value
+	 */
+	const onChangeColumnsNum = ( key, value ) => {
+		onChange( key, value );
+	};
+	/**
+	 * Helper function that returns the column controls.
+	 *
+	 * @return {Array} Array of React Components.
+	 */
+	const columnRangeControls = () => {
+		const columnsControls = [];
 
-    const onChangeColumnsNum = ( value ) => {
-        console.log( 'onChangeColumnsNum', value );
-    }
+		const grid = new LayoutGrid( items, currentDevice, numberOfColumns );
 
-    const columnRangeControls = () => {
-        var columns = [];
-        for ( var i = 1; i < 4; i++ ) {
-            columns.push( 
-            ( <View style={ styles.rangeControl } key={ 'column-' + i }>
-                <Text style={ styles.rangeControlHeading } >{'Column ' + i }</Text>
-                <RangeControl
-                    label={ __( 'Offset' ) }
-                    icon={ columns }
-                    value={ 1 }
-                    onChange={ onChangeColumnsNum }
-                    min={ 0 }
-                    max={ 5 }
-                    type="stepper"
-                    separatorType="none"
-                />
-                <RangeControl
-                    label={ __( 'Span' ) }
-                    icon={ columns }
-                    value={ 1 }
-                    onChange={ onChangeColumnsNum }
-                    min={ 0 }
-                    max={ 5 }
-                    type="stepper"
-                    separatorType="none"
-                />
-            </View>
-            ) );
-        }
-        return columns;
-    }
+		for ( let column = 0; column < numberOfColumns; column++ ) {
+			const offsetKey = getOffsetForDevice( column, currentDevice );
+			const spanKey = getSpanForDevice( column, currentDevice );
+
+			const span =
+				grid.getSpan( column ) ||
+				getDefaultSpan( currentDevice, numberOfColumns, column );
+			const offset = grid.getOffset( column ) || 0;
+
+			const columnNumber = column + 1;
+
+			columnsControls.push(
+				<View
+					style={ styles[ 'range-control' ] }
+					key={ 'column-' + column }
+				>
+					<Text style={ styles[ 'range-control__heading' ] }>
+						{ sprintf(
+							// translators: %d: Inter that identifies the column. For example Column 1.
+							__( 'Column %d', 'layout-grid' ),
+							columnNumber
+						) }
+					</Text>
+					<RangeControl
+						label={ __( 'Offset', 'layout-grid' ) }
+						value={ offset || 0 }
+						onChange={ onChangeColumnsNum.bind( this, offsetKey ) }
+						min={ 0 }
+						max={ getGridWidth( currentDevice ) }
+						type="stepper"
+						separatorType="none"
+					/>
+					<RangeControl
+						label={ __( 'Span', 'layout-grid' ) }
+						value={ span }
+						onChange={ onChangeColumnsNum.bind( this, spanKey ) }
+						min={ 1 }
+						max={ getGridWidth( currentDevice ) + 1 }
+						type="stepper"
+						separatorType="none"
+					/>
+				</View>
+			);
+		}
+
+		return columnsControls;
+	}
+
+	const deviceNames = getLayouts().map( ( layout ) => {
+		return layout.label;
+	} );
 
 	return (
 		<BottomSheet.SubSheet
@@ -100,15 +146,12 @@ const BottomSheetResponsiveBreakpointsControl = ( {
 					screen={ label }
 					leftButtonOnPress={ goBack }
 				/>
-
-                <SegmentedControls 
-                    segments={ [ 'Desktop', 'Tablet', 'Mobile' ] }
-                    segmentHandler= { ( segment ) => { console.log( segment ) } }
-                    selectedIndex={ 0 }
-                />
-                { columnRangeControls() }
-                
-                
+				<SegmentedControls
+					segments={ deviceNames }
+					segmentHandler={ setCurrentDevice }
+					selectedIndex={ 0 }
+				/>
+				{ columnRangeControls() }
 			</>
 		</BottomSheet.SubSheet>
 	);
--- blocks/layout-grid/src/grid/edit.native.js
+++ blocks/layout-grid/src/grid/edit.native.js
@@ -2,8 +2,6 @@
  * External dependencies
  */
 import { View } from 'react-native';
-import { delay, flatMap } from 'lodash';
-
 /**
  * WordPress dependencies
  */
@@ -11,127 +9,302 @@ import {
 	InnerBlocks,
 	InspectorControls,
 	BlockVariationPicker,
+	BlockControls,
+	BlockVerticalAlignmentToolbar,
  } from '@wordpress/block-editor';
  import {
 	PanelBody,
 	ToggleControl,
-	FooterMessageControl,
 	Disabled,
 	BottomSheetSelectControl,
 } from '@wordpress/components';
-import { useEffect, useState } from '@wordpress/element';
+import { Component, createRef, useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import {
-	createBlocksFromInnerBlocksTemplate,
-	store as blockEditorStore,
-} from '@wordpress/blocks';
-import { useDispatch } from '@wordpress/data';
+import { withSelect, withDispatch } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
+import { createBlock } from '@wordpress/blocks';
+
 /**
  * Internal dependencies
  */
 import variations from './variations';
-import { getGutterValues } from './../constants';
-import styles from './edit.scss';
+import { removeGridClasses } from './css-classname';
 import BottomSheetVariationControl from './bottom-sheet-variation-control';
 import BottomSheetResponsiveBreakpointsControl from './bottom-sheet-responsive-breakpoints-control';
+import {
+	DEVICE_BREAKPOINTS,
+	getSpanForDevice,
+	getOffsetForDevice,
+	getGutterValues,
+} from './../constants';
+import { getDefaultSpan } from './grid-defaults';
 
 const ALLOWED_BLOCKS = [ 'jetpack/layout-grid-column' ];
 
-const TEMPLATE = [
+const DEFAULT_TEMPLATE = [
 	[ 'jetpack/layout-grid-column', {}, [] ],
 	[ 'jetpack/layout-grid-column', {}, [] ],
 ];
 
-const Edit = ( props ) => {
-	
-	const { clientId, isSelected, setAttributes, attributes = {} } = props;
-	const [ isVisible, setIsVisible ] = useState( false );
-	const isDefaultColumns = true;
+class Edit extends Component {
 
-	const { gutterSize, addGutterEnds } = attributes;
+	constructor( props ) {
+		super( props );
 
-	useEffect( () => {
-		if ( isSelected && isDefaultColumns ) {
-			delay( () => setIsVisible( true ), 100 );
-		}
-	}, [] );
+		this.overlayRef = createRef();
+		this.state = { isDefaultColumns: ! props.columns };
 
-	const toggleControl = (
-		<ToggleControl
-			label={ __( 'Add end gutters', 'layout-grid' ) }
-			help={
-				addGutterEnds ? __( 'Toggle off to remove the spacing left and right of the grid.', 'layout-grid' ) : __( 'Toggle on to add space left and right of the layout grid. ', 'layout-grid' )
+		this.setNotDefaultColumns = this.setNotDefaultColumns.bind( this );
+		this.onChangeLayout = this.onChangeLayout.bind( this );
+	}
+
+	setNotDefaultColumns() {
+		this.setState( { isDefaultColumns: false } );
+	}
+
+	onChangeLayout( selectedColumn ) {
+		const columnValues = {};
+		const numberOfColumns = selectedColumn.innerBlocks.length;
+		for ( let pos = 0; pos < numberOfColumns; pos++ ) {
+			for (
+				let device = 0;
+				device < DEVICE_BREAKPOINTS.length;
+				device++
+			) {
+				const defaultSpan = getDefaultSpan(
+					DEVICE_BREAKPOINTS[ device ],
+					numberOfColumns,
+					pos
+				);
+
+				columnValues[
+					getSpanForDevice( pos, DEVICE_BREAKPOINTS[ device ] )
+				] = defaultSpan;
+				columnValues[
+					getOffsetForDevice( pos, DEVICE_BREAKPOINTS[ device ] )
+				] = 0;
 			}
-			checked={ addGutterEnds }
-			onChange={ newValue => setAttributes( { addGutterEnds: newValue } )  }
-		/>
-	);
+		}
+		this.setState( { isDefaultColumns: false } );
 
-	return (
-		<>
-			<View>
-				<InnerBlocks
-					template={ TEMPLATE }
-					templateLock="all"
-					allowedBlocks={ ALLOWED_BLOCKS }
-				/>
-			</View>
-			<InspectorControls>
-				<PanelBody title={ __( 'Layouts' ) }>
-					<BottomSheetVariationControl
-						label={ __( 'Columns' ) }
-						value={ 'three-columns' } /* Supply the correct Value */
-						// `undefined` is required for the preload attribute to be unset.
-						onChange={ ( newValue ) => {
-								// @Todo change the number of columns
-								console.log( 'howdy', newValue );
-							}
-							
-						}
-						options={ variations }
-					/>
-					<BottomSheetResponsiveBreakpointsControl
-						label={ __( 'Responsive Breakpoints' ) }
-						value={ 'Edit' } /* Supply the correct Value */
-						// `undefined` is required for the preload attribute to be unset.
-						onChange={ ( newValue ) => {
-								// @Todo change the number of columns
-								console.log( 'howdy', newValue );
-							}
-						}
-						options={ variations }
-					/>
-				</PanelBody>
-				<PanelBody title={ __( 'Layout grid settings' ) }>
-					<BottomSheetSelectControl
-						label={ __( 'Gutters' ) }
-						value={ gutterSize }
-						// `undefined` is required for the preload attribute to be unset.
-						onChange={ ( newValue ) =>
-							setAttributes( {
-								gutterSize: newValue,
-								addGutterEnds:
-									newValue === 'none' ? false : addGutterEnds,
-							} )
-						}
-						options={ getGutterValues() }
-					/>
-					{ gutterSize === 'none' ? (
-						<Disabled>{ toggleControl }</Disabled>
-					) : (
-						toggleControl
-					) }
-				</PanelBody>
-			</InspectorControls>
-			<BlockVariationPicker
-				variations={ variations }
-				onClose={ () => setIsVisible( false ) }
-				clientId={ clientId }
-				isVisible={ isVisible }
+		this.props.updateColumns(
+			this.props.columns,
+			numberOfColumns,
+			columnValues
+		);
+	}
+
+	render() {
+		const {
+			clientId,
+			setAttributes,
+			columns,
+			attributes = {},
+			updateAlignment,
+		} = this.props;
+
+		const { gutterSize, addGutterEnds, verticalAlignment } = attributes;
+
+		const toggleControl = (
+			<ToggleControl
+				label={ __( 'Add end gutters', 'layout-grid' ) }
+				help={
+					addGutterEnds
+						? __(
+								'Toggle off to remove the spacing left and right of the grid.',
+								'layout-grid'
+						  )
+						: __(
+								'Toggle on to add space left and right of the layout grid. ',
+								'layout-grid'
+						  )
+				}
+				checked={ addGutterEnds }
+				onChange={ ( newValue ) =>
+					setAttributes( { addGutterEnds: newValue } )
+				}
 			/>
-		</>
-	);
-};
+		);
 
-export default Edit;
+		return (
+			<>
+				<View>
+					<InnerBlocks
+						template={
+							this.state.isDefaultColumns
+								? DEFAULT_TEMPLATE
+								: null
+						}
+						templateLock="all"
+						allowedBlocks={ ALLOWED_BLOCKS }
+					/>
+				</View>
+				<InspectorControls>
+					<PanelBody title={ __( 'Layouts', 'layout-grid' ) }>
+						<BottomSheetVariationControl
+							label={ __( 'Columns', 'layout-grid' ) }
+							value={ columns }
+							onChange={ this.onChangeLayout }
+							options={ variations }
+						/>
+						<BottomSheetResponsiveBreakpointsControl
+							label={ __(
+								'Responsive breakpoints',
+								'layout-grid'
+							) }
+							value={ __( 'Edit', 'layout-grid' ) }
+							onChange={ ( key, newValue ) => {
+								const data = {};
+								data[ key ] = newValue;
+								setAttributes( data );
+							} }
+							columns={ columns }
+							options={ attributes }
+						/>
+					</PanelBody>
+					<PanelBody
+						title={ __( 'Layout grid settings', 'layout-grid' ) }
+					>
+						<BottomSheetSelectControl
+							label={ __( 'Gutters', 'layout-grid' ) }
+							value={ gutterSize }
+							// `undefined` is required for the preload attribute to be unset.
+							onChange={ ( newValue ) =>
+								setAttributes( {
+									gutterSize: newValue,
+									addGutterEnds:
+										newValue === 'none'
+											? false
+											: addGutterEnds,
+								} )
+							}
+							options={ getGutterValues() }
+						/>
+						{ gutterSize === 'none' ? (
+							<Disabled>{ toggleControl }</Disabled>
+						) : (
+							toggleControl
+						) }
+					</PanelBody>
+				</InspectorControls>
+				<BlockControls>
+					<BlockVerticalAlignmentToolbar
+						onChange={ updateAlignment }
+						value={ verticalAlignment }
+					/>
+				</BlockControls>
+				<BlockVariationPicker
+					variations={ variations }
+					onClose={ this.setNotDefaultColumns }
+					clientId={ clientId }
+					isVisible={ this.state.isDefaultColumns }
+				/>
+			</>
+		);
+	}
+}
+
+function getColumnBlocks( currentBlocks, previous, columns ) {
+	if ( columns > previous ) {
+		// Add new blocks to the end
+		return [
+			...currentBlocks,
+			...Array.from( { length: columns - previous }, () =>
+				createBlock( 'jetpack/layout-grid-column' )
+			),
+		];
+	}
+
+	// A little ugly but... ideally we remove empty blocks first, and then anything with content from the end
+	let cleanedBlocks = [ ...currentBlocks ];
+	let totalRemoved = 0;
+
+	// Reverse the blocks so we start at the end. This happens in-place
+	cleanedBlocks.reverse();
+
+	// Remove empty blocks
+	cleanedBlocks = cleanedBlocks.filter( ( block ) => {
+		if (
+			totalRemoved < previous - columns &&
+			block.innerBlocks.length === 0
+		) {
+			totalRemoved++;
+			return false;
+		}
+
+		return true;
+	} );
+
+	// If we still need to remove blocks then do them from the beginning before flipping it back round
+	return cleanedBlocks
+		.slice( Math.max( 0, previous - columns - totalRemoved ) )
+		.reverse();
+}
+
+export default compose( [
+	withDispatch( ( dispatch, ownProps, registry ) => ( {
+		/**
+		 * Update all child Column blocks with a new vertical alignment setting
+		 * based on whatever alignment is passed in. This allows change to parent
+		 * to overide anything set on a individual column basis.
+		 *
+		 * @param {string} verticalAlignment the vertical alignment setting
+		 */
+		updateAlignment( verticalAlignment ) {
+			const { clientId, setAttributes } = ownProps;
+			const { updateBlockAttributes } = dispatch( 'core/block-editor' );
+			const { getBlockOrder } = registry.select( 'core/block-editor' );
+
+			// Update own alignment.
+			setAttributes( { verticalAlignment } );
+
+			// Update all child Column Blocks to match
+			const innerBlockClientIds = getBlockOrder( clientId );
+			innerBlockClientIds.forEach( ( innerBlockClientId ) => {
+				updateBlockAttributes( innerBlockClientId, {
+					verticalAlignment,
+				} );
+			} );
+		},
+		updateColumns( previous, columns, columnValues ) {
+			const { clientId } = ownProps;
+			const { replaceBlock } = dispatch( 'core/block-editor' );
+			const { getBlocks } = registry.select( 'core/block-editor' );
+			const innerBlocks = getColumnBlocks(
+				getBlocks( clientId ),
+				previous,
+				columns
+			);
+
+			// Replace the whole block with a new one so that our changes to both the attributes and innerBlocks are atomic
+			// This ensures that the undo history has a single entry, preventing traversing to a 'half way' point where innerBlocks are changed
+			// but the column attributes arent
+			const blockCopy = createBlock(
+				ownProps.name,
+				{
+					...ownProps.attributes,
+					...columnValues,
+					className: removeGridClasses(
+						ownProps.attributes.className
+					),
+				},
+				innerBlocks
+			);
+
+			replaceBlock( clientId, blockCopy );
+		},
+	} ) ),
+
+	withSelect( ( select, { clientId } ) => {
+		const { getBlockOrder, getBlockCount, getBlocksByClientId } = select(
+			'core/block-editor'
+		);
+		return {
+			columns: getBlockCount( clientId ),
+			columnAttributes: getBlockOrder( clientId ).map(
+				( innerBlockClientId ) =>
+					getBlocksByClientId( innerBlockClientId )[ 0 ].attributes
+			),
+		};
+	} ),
+] )( Edit );
