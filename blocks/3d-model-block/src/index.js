9a649fda5c0a5c215287f4552d8edf78d66f0dd0
Alex Lende <alex@lende.xyz>
2022-04-26 12:32:15 -0500

    Updating 3d model source to match project

--- blocks/3d-model-block/src/index.js
+++ blocks/3d-model-block/src/index.js
@@ -1,37 +1,21 @@
 /**
- * Registers a new block provided a unique name and an object defining its behavior.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * All files containing `style` keyword are bundled together. The code used
- * gets applied both to the front of your site and to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './style.scss';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import Edit from './edit';
-import save from './save';
+import Save from './save';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
-registerBlockType( 'create-block/3d-model-block', {
-	/**
-	 * @see ./edit.js
-	 */
-	edit: Edit,
-	/**
-	 * @see ./save.js
-	 */
-	save,
-} );
+export const registerBlock = () => {
+	registerBlockType( 'a8c/32-model-block', {
+		title: '3d model viewer',
+		description: __( 'Jazz it up!' ),
+		category: 'widgets',
+		edit: ( props ) => <Edit { ...props } />,
+		save: ( props ) => <Save { ...props } />,
+	} );
+};
