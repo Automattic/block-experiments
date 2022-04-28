bf073ccbd41dc9504ce1c02acc0286980ac2b746
Alex Lende <alex@lende.xyz>
2022-04-28 14:25:23 -0500

    Save width and height

--- blocks/model-block/src/save.js
+++ blocks/model-block/src/save.js
@@ -1,34 +1,25 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
- */
 import { useBlockProps } from '@wordpress/block-editor';
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
- *
- * @return {WPElement} Element to render.
- */
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH } from './utils.js';
+
 export default function save( { attributes } ) {
-	const { src, alt } = attributes;
+	const { src, alt, width, height, widthUnit, heightUnit } = attributes;
 
 	return (
 		<figure { ...useBlockProps.save() }>
 			<model-viewer
 				class="wp-block-a8c-model-viewer__component"
+				style={ {
+					height:
+						height && heightUnit
+							? `${ height }${ heightUnit }`
+							: DEFAULT_HEIGHT,
+					width:
+						width && widthUnit
+							? `${ width }${ widthUnit }`
+							: DEFAULT_WIDTH,
+				} }
 				alt={ alt }
 				src={ src }
 				ar="true"
