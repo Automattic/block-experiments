9fdc34cd30258889c2b1f0c1f3d8a8c898322dfa
oskosk <oskosk@gmail.com>
2023-03-01 08:56:01 -0300

    Update getSvgPathFromStroke
    ...to match the one currently published in Perfect Freehand README (March 1st 2023)
    
    https://github.com/steveruizok/perfect-freehand/blob/58c5fce6936c9b67dfd21fada83266dc988a01a1/README.md#enroll-beta

--- blocks/sketch/src/lib.js
+++ blocks/sketch/src/lib.js
@@ -19,20 +19,34 @@ export const presets = [
 	},
 ];
 
+const average = (a, b) => (a + b) / 2
+
+
 // Create SVG path data using the strokes from perfect-freehand.
-export function getSvgPathFromStroke( stroke ) {
-	if ( stroke.length === 0 ) return '';
+export function getSvgPathFromStroke( points, closed = true ) {
+	const len = points.length
 
-	const d = stroke.reduce(
-		( acc, [ x0, y0 ], i, arr ) => {
-			const [ x1, y1 ] = arr[ ( i + 1 ) % arr.length ];
-			acc.push( x0, y0, ( x0 + x1 ) / 2, ( y0 + y1 ) / 2 );
-			return acc;
-		},
-		[ 'M', ...stroke[ 0 ], 'Q' ]
-	);
+	if (len < 4) {
+	  return ``
+	}
 
-	d.push( 'Z' );
+	let a = points[0]
+	let b = points[1]
+	const c = points[2]
 
-	return d.join( ' ' );
+	let result = `M${a[0].toFixed(2)},${a[1].toFixed(2)} Q${b[0].toFixed(2)},${b[1].toFixed(
+	  2
+	)} ${average(b[0], c[0]).toFixed(2)},${average(b[1], c[1]).toFixed(2)} T`
+
+	for (let i = 2, max = len - 1; i < max; i++) {
+	  a = points[i]
+	  b = points[i + 1]
+	  result += `${average(a[0], b[0]).toFixed(2)},${average(a[1], b[1]).toFixed(2)} `
+	}
+
+	if (closed) {
+	  result += 'Z'
+	}
+
+	return result
 }
