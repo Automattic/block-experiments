6474857ecb8445f480c879710b4530df8796bf95
Jerry Jones <jones.jeremydavid@gmail.com>
2025-03-19 14:25:15 -0500

    Remove seemingly unneeded plugin override from babel config

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,5 @@ module.exports = ( api ) => {
 	return {
 		presets: [ '@wordpress/babel-preset-default' ],
 		plugins: [ '@emotion/babel-plugin', 'babel-plugin-inline-json-import' ],
-		overrides: [
-			{
-				plugins: [
-					require.resolve( '@wordpress/block-library/babel-plugin' ),
-				],
-			},
-		],
 	};
 };
